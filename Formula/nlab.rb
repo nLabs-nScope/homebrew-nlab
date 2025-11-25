e99c0d94adfdb2b95e9c4778fcb674b0e4a7463e
David Meyer <david@nscope.org>
2025-11-25 15:26:47 -0800

    fix bug in asset url

--- Formula/nlab.rb
+++ Formula/nlab.rb
@@ -5,10 +5,10 @@ class Nlab < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/nLabs-nScope/nLab/releases/download/v2.0.0/nLab-darwin-arm64-2.0.0.zip"
+      url "https://github.com/nLabs-nScope/nLab/releases/download/2.0.0/nLab-darwin-arm64-2.0.0.zip"
       sha256 "f8c44ec5a71e01e087b6772f9bd8d80753c1652018f17c8b0241bf7ae4cec973"
     else
-      url "https://github.com/nLabs-nScope/nLab/releases/download/v2.0.0/nLab-darwin-x64-2.0.0.zip"
+      url "https://github.com/nLabs-nScope/nLab/releases/download/2.0.0/nLab-darwin-x64-2.0.0.zip"
       sha256 "f8c44ec5a71e01e087b6772f9bd8d80753c1652018f17c8b0241bf7ae4cec973"
     end
   end
